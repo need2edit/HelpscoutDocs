49aa4def671c5e3ec8ccdec8f029680f4b5881c1
Jake Young <jake@acpdecisions.org>
2018-10-04 13:03:19 -0400

    adds alamofire as dependency

--- Sources/HelpscoutDocs/HelpscoutDocs.swift
+++ Sources/HelpscoutDocs/HelpscoutDocs.swift
@@ -1,3 +1,21 @@
-struct HelpscoutDocs {
-    var text = "Hello, World!"
+import Alamofire
+
+
+public class HelpscoutDocs {
+    
+    let api_password: String
+    
+    public init(api_password: String) {
+        self.api_password = api_password
+    }
+    
+    public func listArticles(completionHandler: ([HSArticleRef]) -> Void) {
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response)
+        }
+        
+    }
+    
+    
 }
